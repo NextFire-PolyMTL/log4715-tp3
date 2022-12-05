5a97eacef2afa61e49f143d30b96dae438a4219d
Nicolas <nicolas.kaikati@gmail.com>
2022-12-05 11:57:46 -0500

    scène 1 avec buttoncontroler2

---
+++ Assets/0000 - NIVEAU FINAL/9 - COLLABORATION/ButtonController2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ToggledObject;
+    [SerializeField]
+    private GameObject _ToggledObject2;
+    private bool active=true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is on top of the button
+        if (collision.contacts[0].normal.y < 0)
+        {
+            active=!active;
+            _ToggledObject.SetActive(active);
+            _ToggledObject2.SetActive(active);
+            var renderer = transform.GetComponent<Renderer>();
+            if (active==false){
+                renderer.material.color = Color.green;
+            }else{
+                renderer.material.color = Color.red;
+            }
+            
+        }
+    }
+}
