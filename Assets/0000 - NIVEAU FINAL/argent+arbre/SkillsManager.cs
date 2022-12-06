6d2181d801ec8c4e685fe1ffeef3c0b01a4e189d
Nicolas <nicolas.kaikati@gmail.com>
2022-12-06 15:13:24 -0500

    menu unlocked niv 5

--- Assets/0000 - NIVEAU FINAL/argent+arbre/SkillsManager.cs
+++ Assets/0000 - NIVEAU FINAL/argent+arbre/SkillsManager.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public enum Skill
 {
     DoubleSaut,
-    Skill1,
+    Dash,
     Skill2,
     Skill3,
     Skill4,
@@ -27,6 +27,7 @@ public class SkillsManager : MonoBehaviour
     [SerializeField] private Text _txt;
 
     [SerializeField] private TMP_Text _txt2;
+    public bool set_arbre;
 
 
     // Start is called before the first frame update
@@ -40,6 +41,13 @@ public class SkillsManager : MonoBehaviour
     {
         _txt.text = XP.ToString();
         _txt2.text = XP.ToString();
+        Scene scene = SceneManager.GetActiveScene(); 
+        //Debug.Log("scene");
+        if (scene.name=="1 - previllage" || scene.name=="2 - portes boutons" || scene.name=="3 - Boue et Piege" || scene.name=="4 - plateforme mouvante"){
+            set_arbre=false;
+        } else{
+            set_arbre=true;
+        }
     }
 
 
