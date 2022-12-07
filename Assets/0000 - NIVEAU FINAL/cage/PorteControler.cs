0a430fd42a0532774dc4917de2af7876dd9e061b
Nicolas <nicolas.kaikati@gmail.com>
2022-12-07 11:46:08 -0500

    fix

--- Assets/0000 - NIVEAU FINAL/cage/PorteControler.cs
+++ Assets/0000 - NIVEAU FINAL/cage/PorteControler.cs
@@ -8,10 +8,10 @@ public class PorteControler : MonoBehaviour
     [SerializeField] private Animator _prisonnier;
     [SerializeField] private SkillsManager _skillsManager;
     //[SerializeField] private GameObject _successMessage;
-     private Animator garde1;
-     private Animator garde2;
-     private Animator garde3;
-     private Animator garde4;
+     //private Animator garde1;
+     //private Animator garde2;
+     //private Animator garde3;
+     //private Animator garde4;
     //[SerializeField] private GameObject p;
     
     
@@ -19,10 +19,10 @@ public class PorteControler : MonoBehaviour
 
     void Start()
     {
-        garde1=GameObject.Find("Top/Characters/garde1").GetComponent<Animator>();
-        garde2=GameObject.Find("Top/Characters/garde2").GetComponent<Animator>();
-        garde3=GameObject.Find("Top/Characters/garde3").GetComponent<Animator>();
-        garde4=GameObject.Find("Top/Characters/garde1 (1)").GetComponent<Animator>();
+        //garde1=GameObject.Find("Top/Characters/garde1").GetComponent<Animator>();
+        //garde2=GameObject.Find("Top/Characters/garde2").GetComponent<Animator>();
+       // garde3=GameObject.Find("Top/Characters/garde3").GetComponent<Animator>();
+        //garde4=GameObject.Find("Top/Characters/garde1 (1)").GetComponent<Animator>();
         
 
     }
@@ -40,17 +40,17 @@ public class PorteControler : MonoBehaviour
             gameObject.SetActive(false);
             //_prisonnier.Play("Avance", 0, 0.0f);
             
-            SkillsManager.XP += 75;
+            //SkillsManager.XP += 200;
             //_successMessage.SetActive(true);
 
         }
         if (coll.gameObject.tag == "Prisoner")
         {
             _porte.Play("Fermeture", 0, 0.0f);
-            garde1.Play("Danse",0,0.0f);
-            garde2.Play("Danse",0,0.0f);
-            garde3.Play("Danse",0,0.0f);
-            garde4.Play("Danse",0,0.0f);
+            //garde1.Play("Danse",0,0.0f);
+           // garde2.Play("Danse",0,0.0f);
+          //  garde3.Play("Danse",0,0.0f);
+          //  garde4.Play("Danse",0,0.0f);
             _prisonnier.Play("Danse",0,0.0f);
             //prisonnier.Play("idle");
             gameObject.SetActive(false);
