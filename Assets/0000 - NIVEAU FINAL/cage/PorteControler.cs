b9563e5af41533979488ba7961b8a179990848bc
Nicolas <nicolas.kaikati@gmail.com>
2022-12-07 12:24:44 -0500

    niv 10 animation

--- Assets/0000 - NIVEAU FINAL/cage/PorteControler.cs
+++ Assets/0000 - NIVEAU FINAL/cage/PorteControler.cs
@@ -7,11 +7,12 @@ public class PorteControler : MonoBehaviour
     [SerializeField] private Animator _porte;
     [SerializeField] private Animator _prisonnier;
     [SerializeField] private SkillsManager _skillsManager;
+    private bool refaire=true;
     //[SerializeField] private GameObject _successMessage;
-     //private Animator garde1;
-     //private Animator garde2;
-     //private Animator garde3;
-     //private Animator garde4;
+     private Animator garde1;
+     private Animator garde2;
+     private Animator garde3;
+     private Animator garde4;
     //[SerializeField] private GameObject p;
     
     
@@ -19,10 +20,10 @@ public class PorteControler : MonoBehaviour
 
     void Start()
     {
-        //garde1=GameObject.Find("Top/Characters/garde1").GetComponent<Animator>();
-        //garde2=GameObject.Find("Top/Characters/garde2").GetComponent<Animator>();
-       // garde3=GameObject.Find("Top/Characters/garde3").GetComponent<Animator>();
-        //garde4=GameObject.Find("Top/Characters/garde1 (1)").GetComponent<Animator>();
+        garde1=GameObject.Find("Top/Characters/garde1").GetComponent<Animator>();
+        garde2=GameObject.Find("Top/Characters/garde2").GetComponent<Animator>();
+        garde3=GameObject.Find("Top/Characters/garde3").GetComponent<Animator>();
+        garde4=GameObject.Find("Top/Characters/garde1 (1)").GetComponent<Animator>();
         
 
     }
@@ -34,27 +35,22 @@ public class PorteControler : MonoBehaviour
 
     private void OnTriggerEnter(Collider coll)
     {
-        if (coll.gameObject.tag == "Hero")
+        if (coll.gameObject.tag == "Hero" && refaire)
         {
             _porte.Play("Ouverture", 0, 0.0f);
+            refaire=false;
             gameObject.SetActive(false);
             //_prisonnier.Play("Avance", 0, 0.0f);
             
             //SkillsManager.XP += 200;
             //_successMessage.SetActive(true);
+            garde1.Play("Danse",0,0.0f);
+            garde2.Play("Danse",0,0.0f);
+            garde3.Play("Danse",0,0.0f);
+            garde4.Play("Danse",0,0.0f);
+            _prisonnier.Play("Danse",0,0.0f);
 
         }
-        if (coll.gameObject.tag == "Prisoner")
-        {
-            _porte.Play("Fermeture", 0, 0.0f);
-            //garde1.Play("Danse",0,0.0f);
-           // garde2.Play("Danse",0,0.0f);
-          //  garde3.Play("Danse",0,0.0f);
-          //  garde4.Play("Danse",0,0.0f);
-            _prisonnier.Play("Danse",0,0.0f);
-            //prisonnier.Play("idle");
-            gameObject.SetActive(false);
-            //_successMessage.SetActive(false);
-        }
+        
     }
 }
